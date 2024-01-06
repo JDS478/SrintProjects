b5eba1bba910a5d9b73995aed0a5e394ebadc41f
Joel Savage <joel28169@gmail.com>
2024-01-06 16:38:13 +0000

    Added registrations form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,11 +9,28 @@ class RegistrationsController < ApplicationController
 
   def login; end
 
-  def loginuser; end
+  def loginuser
+    user = User.find_by(email: params[:email])
+    # Authenticate method comes from bcrypt gem for user model
+    if user.present? && user.authenticate(params[:password])
+      flash[:notice] = 'Signed in successfully!'
+      handle_login(user)
+
+      redirect_to root_path
+    else
+      flash[:alert] = 'Password is incorrect or no user found!'
+
+      redirect_to registrations_login_path
+    end
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:username, :display_name, :password, :password_confirmation)
+  end
+
+  def handle_login(user)
+    session[:token].signed = user.id
   end
 end
